963d1114fcea010dc4a02c38db16806b97326ed5
Kaveenxz <kaveenhansithx@gmail.com>
2024-01-18 21:33:40 +0530

    add yml file with hibernate

--- src/main/java/edu/icet/service/BookService.java
+++ src/main/java/edu/icet/service/BookService.java
@@ -3,7 +3,6 @@ package edu.icet.service;
 import edu.icet.dto.Book;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface BookService {
     public void addBook(Book book);
 }
